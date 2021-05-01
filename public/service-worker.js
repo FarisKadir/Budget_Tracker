619c4e12c12a953c5e9c17d1915186528d2d9f66
Faris Kadir <faris.kadir@gmail.com>
2021-05-01 18:55:27 -0400

    created indexeddb to store data when offline.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,63 +1,76 @@
+console.log("You service worker is running!");
+
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/index.js',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-  ];
-  
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime-cache';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+  `/`,
+  `/index.html`,
+  `/indexedDB.js`,
+  `/index.js`,
+  `/manifest.webmanifest`,
+  `/icons/icon-192x192.png`,
+  `/icons/icon-512x512.png`,
+  `/styles.css`,
+];
+
+const CACHE_NAME = `static-cache-v1`;
+const DATA_CACHE_NAME = `data-cache-v1`;
+
+// Install
+self.addEventListener(`install`, (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log(`Your files were pre-cached successfully!`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener(`activate`, (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log(`Removing old cache data`, key);
+            return caches.delete(key);
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+          return undefined;
         })
-      );
-    }
-  });
-  
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener(`fetch`, (evt) => {
+  if (evt.request.url.includes(`/api/`)) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) =>
+          fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            // Network request failed, try to get it from the cache.
+            .catch(() => cache.match(evt.request))
+        )
+        .catch((err) => console.log(err))
+    );
+  } else {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then((response) => response || fetch(evt.request))
+    );
+  }
+});
